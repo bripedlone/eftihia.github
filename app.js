ac1e255f90e09559e6f9af1b657a9d88fd27639a
Brisa Pedlone <brichupedlone03@gmail.com>
2022-09-29 18:20:36 -0300

    corrigiendo js

--- app.js
+++ app.js
@@ -1,183 +1,153 @@
-const shop = document.getElementById("shopContent");
-const modalContainer = document.getElementById("modal-container")
-const contenedorCarrito =ndocument.getElementById("carrito-contenedor")
-const botonVaciar = document.getElementById("vaciar-carrito")
-const precioTotal = document.getElementById("precioTotal")
+const products = [
+    {
+        "id": 1,
+        "name": "Crimen y castigo",
+        "price": 3799,
+        "img": "./img/crimenycastigo-clasico.jpg",
+    },
+    {
+        "id": 2,
+        "name": "El retrato de Dorian Gray",
+        "price": 2099,
+        "img": "./img/doriangray-clasico.jpg"
+    },
+    {
+        "id": 3,
+        "name": "La metamorfosis",
+        "price": 4000,
+        "img": "./img/metamorfosis-clasico.jpg",
+    },
+    {
+        "id": 4,
+        "name": "1984",
+        "price": 3099,
+        "img": "./img/1984-ing.jpg"
+    },
+    {
+        "id": 5,
+        "name": "The goldfinch",
+        "price": 6956,
+        "img": "./img/goldfinch-ing.jpg"
+    },
+    {
+        "id": 6,
+        "name": "The secret history",
+        "price": 2776,
+        "img": "./img/secrethistory-ing.jpg",
+    },
+    {
+        "id": 7,
+        "name": "If we were villains",
+        "price": 6956,
+        "img": "./img/villains-ing.jpg",
+    },
+    {
+        "id": 8,
+        "name": "Noches blancas",
+        "price": 2839,
+        "img": "./img/nochesblacas-rusa.jpg",
+    },
+    {
+        "id": 9,
+        "name": "Lolita",
+        "price": 3500,
+        "img": "./img/lolita-rusa.jpg",
+    },
+    {
+        "id": 10,
+        "name": "Los hermanos Karamazov",
+        "price": 4299,
+        "img": "./img/karamazov-rusa.jpg",
+    },
+    {
+        "id": 11,
+        "name": "Sherlock Holmes",
+        "price": 4442,
+        "img": "./img/sherlockholmes-ing.jpg",
+    },
+    {
+        "id": 12,
+        "name": "Los crímenes de la calle Morgue",
+        "price": 1752,
+        "img": "./img/morgue-poli.jpg",
+    },
+    {
+        "id": 13,
+        "name": "El maestro y Margarita",
+        "price": 2570,
+        "img": "./img/margarita-rusa.jpg",
+    },
+    {
+        "id": 14,
+        "name": "Orgullo y prejuicio",
+        "price": 4500,
+        "img": "./img/orgulloyprejuicio-clasico.jpg",
+    },
+]  
 
-let carrito = []
+const dibujarProducto = ()=> {
+    let contenedor = document.getElementById("container")
+    products.forEach((product, indice) => {
+        let card = document.createElement("div")
+        card.classList.add("card", "col-sm-12", "col-lg-3");
+        card.innerHTML += `<div class="card" style="width: 18rem;">
+        <img src="${shopContent.img}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${product.name}</h5>
+          <p class="card-text">${product.price}</p>
+          <a href="#" class="btn btn-buy" onclick="agregarAlCarrito()">Buy</a>
+        </div>`;
+        contenedor.appendChild(card)
+    });
+}
+dibujarProducto();
 
-document.addEventListener("DOMContentLoaded", () => {
-    if(localStorage.getItem(`carrito`)){
-        carrito = JSON.parse(localStorage.getItem(`carrito`))
-        actualizarCarrito()
+let cart = []
+let modalCart = document.getElementById("cart") 
+
+const agregarAlCarrito = (indice) =>{
+    const indiceFindCart = cart.findIndex((element)=>{
+        return element.id === product[indice].id
+    });
+    if(indiceFindCart === -1){
+        const productAdd = products[indice]
+        productAdd.quantity = 1;
+        cart.push(productAdd);
+        dibujarCarrito()
+    }else{
+        cart[indiceFindCart].quantity +=1;
+        dibujarCarrito(); localStorage.getItem("productos añadidos")
     }
-})
-
-botonVaciar.addEventListener("click", () =>{
-    carrito.length = 0
-    actualizarCarrito()
-})
-
-let shopContent = [
-    {
-        id: 1,
-        nombre: "Crimen y castigo",
-        precio: 3.799,
-        img: "../eftihia/img/crimenycastigo-clasico.jpg",
-    },
-    {
-        id: 2, 
-        nombre: "El retrato de Dorian Gray",
-        precio: 2099,
-        img: "../eftihia/img/doriangray-clasico.jpg"
-    },
-    {
-        id: 3,
-        nombre: "La metamorfosis",
-        precio: 4000,
-        img: "../eftihia/img/metamorfosis-clasico.jpg",
-    },
-    {
-        id: 4,
-        nombre: "Orgullo y prejuicio",
-        precio: 4420,
-        img: "../eftihia/img/orgulloyprejuicio-clasico.jpg",
-    },
-    {
-        id: 5,
-        nombre: "Los hermanos Karamazov",
-        precio: 4299,
-        img: "../eftihia/img/karamazov-rusa.jpg",
-    },
-    {
-        id: 6,
-        nombre: "El maestro y la margarita",
-        precio: 4300,
-        img: "../eftihia/img/margarita-rusa.jpg",
-    },
-    {
-        id: 7,
-        nombre: "Lolita",
-        precio: 3500,
-        img: "../eftihia/img/lolita-rusa.jpg",
-    },
-    {
-        id: 8,
-        nombre: "Noches blancas",
-        precio: 2839,
-        img: "../eftihia/img/nochesblacas-rusa.jpg",
-    },
-    {
-        id: 9,
-        nombre: "The secret history",
-        precio: 2776,
-        img:"../eftihia/img/secrethistory-ing.jpg"
-    },
-    {
-        id: 10,
-        nombre: "1984",
-        precio: 3999,
-        img: "../eftihia/img/1984-ing.jpg",
-    },
-    {
-        id: 11,
-        nombre: "The goldfinch",
-        precio: 6956,
-        img: "../eftihia/img/goldfinch-ing.jpg",
-    },
-    {
-        id: 12,
-        nombre: "If we were villains",
-        precio: 2418,
-        img:"../eftihia/img/villains-ing.jpg",
-    },
-    {
-        id: 13,
-        nombre: "Sherlock Holmes",
-        precio: 4449,
-        img: "../eftihia/img/sherlockholmes-ing.jpg" ,
-    },
-    {
-        id: 14,
-        nombre: "El nombre de la rosa",
-        precio: 2849,
-        img: "../eftihia/img/elnombredelarosa.jpg" ,
-    },
-    {
-        id: 15,
-        nombre: "Los crimenes de la calle morgue",
-        precio: 1752,
-        img: "../eftihia/img/morgue-poli.jpg",
-    },
-    {
-        id: 16, 
-        nombre: "El espía que surgió del frío",
-        precio: 2295,
-        img: "../eftihia/img/thespy-poli.jpg",
-    },
-]
-
-productos.forEach((product)=>{
-    let content = document.createElement("div");
-    content.className = "card";
-    content.innerHTML = `
-    <img src="${product.img}">
-    <h3 class="tituloClasic">${product.nombre}</h3>
-    <p class="price">$${product.precio}</p>
-    `;
-
-    shopContent.append(content);
-
-    const boton = document.getElementById(`Agregar ${producto.id}`)
-
-    boton.addEventListener("click", () => {
-        agregarAlCarrito(producto.id)
-    })
-    })
+}
 
 
-const agregarAlCarrito = (prodId) => {
-    const existe = carrito.some (prod => prod.id === prodId)
-
-    if (existe){
-        const prod = carrito.map(prod => {
-            if (prod.id === prodId){
-                prod.cantidad++
-            }
+let total = 0
+const dibujarCarrito = () =>{
+    modalCart.className = "cartt"
+    modalCart.innerHTML = "";
+    if(cart.length > 0){
+        cart.forEach((product, indice) =>{
+            total = total + product.price * product.quantity; localStorage.getItem("producto total")
+            const cartContainer = document.createElement("div");
+            cartContainer.className = "cart-product";
+            cartContainer.innerHTML = `
+            <img class= "cart-img" src= "${product.img}"/>
+            <div class= "product-details">
+            ${product.name}
+            </div>
+            <div class= "product-details"> Quantity: ${product.quantity}</div>
+            <div class= "product-details"> Price: ${product.price}</div>
+            <duv class= "product-details">Subtotal: ${product.price * product.quantity}</div> 
+            <button class= "btn btn-info" id="remove-product" onClick="removeProduct(${indice})"Delete products</button>`; localStorage.getItem("carrito")
+            modalCart.appendChild(cartContainer);
         })
-    } else {
-
-    const item = stockProductos.find ((prod) => prod.Id === prodId)
-    carrito.push(item)
-    console.log(carrito)
+        const totalContainer = document.createElement("div")
+        totalContainer.className= "total-cart"
+        totalContainer.innerHTML= `<div class= "total">Total $ ${total}</div>
+        <button class= "btn btn-info finalizar" id= "finalizar" onClick="finalizarCompra`; localStorage.getItem("fin compra")
+    }
 }
-actualizarCarrito()
-
-const elimicarDelCarrito = (prodId) => {
-    const item = carrito.find((prod) =>prod.id === prodId)
-    const indice = carrito.indexOf(item)
-    carrito.splice(indice, 1)
-    actualizarCarrito()
-}
-
-const actualizarCarrito = () =>{
-
-    contenedorCarrito.innerHTML = ""
-
-    carrito.forEach((prod) =>{
-        const div = document.createElement("div")
-        div.className= "producto-carrito"
-        div.innerHTML = `
-        <p>${prod.nombre}</p>
-        <p>${prod.precio}</p>
-        <p>Cantidad: >span id="cantidad>${prod.cantidad}</span></p>
-        >button onclick = "eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></button>
-        `
-        contenedorCarrito.appendChild(div)
-
-        localStorage.setItem(`carrito`, JSON.stringify(carrito))
-        contadorCarrito.innerText = carrito.length
-        precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
-    
-    })
+const removeProduct = (indice)=> {
+    cart.splice(indice, 1);
+    dibujarCarrito();
 }
